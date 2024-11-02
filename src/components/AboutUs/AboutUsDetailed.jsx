9829cf7d88abb52e7481d4465b073176e11d9ae6
rashedsabbir <rashedulhasan1996@gmail.com>
2024-11-03 00:24:02 +0600

    mission vision added

--- src/components/AboutUs/AboutUsDetailed.jsx
+++ src/components/AboutUs/AboutUsDetailed.jsx
@@ -85,50 +85,57 @@ const AboutUsDetailed = () => {
           <div className="grid grid-cols-12 gap-5 justify-center items-center">
             <div className="col-span-12 lg:col-span-6">
               <div className="text-center lg:px-20">
-                <p className="opacity-75 uppercase">TEMPLATE OF EASYFRONTEND</p>
                 <h1 className="text-4xl md:text-7xl leading-tight font-light uppercase tracking-wide">
-                  DESIGN & <span className="font-bold">PASSION</span>
-                  <span className="inline-flex w-3 h-3 rounded-full bg-blue-600 ml-2"></span>
+                  MISSION & <span className="font-bold">VISION</span>
+                  <span className="inline-flex w-3 h-3 rounded-full bg-blue-500 ml-2"></span>
                 </h1>
-                <p className="text-xl leading-normal opacity-75 mt-4 mb-6">
-                  Completely network collaborative web services via user-centric
-                  initiatives. Quickly promote sticky testing procedures
-                  collaborator before unique process improvements.
-                </p>
-                <p className="opacity-50 text-sm lg:mx-12">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Voluptates, sunt dignissimos veritatis possimus assumenda
-                  libero officiis accusamus ipsum blanditiis.
+                <p className="text-xl leading-normal opacity-75 mt-4 mb-6 text-justify">
+                  At Blockchain Technology, our mission is to empower businesses
+                  by providing innovative, customized software solutions and
+                  comprehensive IT services that enhance operational efficiency,
+                  bolster security, and drive digital transformation. We are
+                  committed to delivering exceptional value through our
+                  expertise in technology consulting, cybersecurity, and digital
+                  marketing, ensuring our clients stay ahead in an ever-evolving
+                  digital landscape.
                 </p>
               </div>
             </div>
             <div className="col-span-12 lg:col-span-6">
-              <div className="lg:ml-12">
-                <p className="opacity-75 mb-4">
-                  <span className="text-[40px] text-blue-600 font-bold">W</span>
-                  eb services via user-centric initiatives. Quickly promote
-                  sticky testing procedures before unique process improvements.
-                  Distinctively engineer innovative information whereas
-                  revolutionary process improvements. Lorem ipsum dolor sit
-                  amet, consectetur adipisicing elit. Quia enim omnis saepe
-                  dolor voluptatum.
+              <div className=" lg:mx-12">
+                <p className="opacity-75 mb-4 text-justify">
+                  <span className="text-[40px] text-blue-500 font-bold ">
+                    O
+                  </span>
+                  ur vision is to be a leading provider of cutting-edge
+                  technology solutions that revolutionize the way businesses
+                  operate. We aspire to create a secure, efficient, and
+                  interconnected world where organizations can harness the full
+                  potential of technology to achieve their goals and foster
+                  sustainable growth. Through continuous innovation and a
+                  customer-centric approach, we aim to inspire trust and become
+                  the partner of choice for businesses seeking to thrive in the
+                  digital age.
                 </p>
-                <p className="opacity-75 mb-4">
-                  Distinctively engineer innovative information whereas
-                  revolutionary process improvements. Lorem ipsum dolor sit
-                  amet, consectetur adipisicing elit. Quia enim omnis saepe
-                  dolor voluptatum. Natus soluta maxime ipsum nam sapiente
-                  dignissimos voluptatum totam.
+                <p className="opacity-75 mb-4 text-justify">
+                  We aspire to create a secure and interconnected world where
+                  businesses leverage blockchain technology, artificial
+                  intelligence, and data analytics to make informed decisions
+                  and drive innovation. By fostering a culture of collaboration,
+                  creativity, and continuous learning, we aim to build
+                  long-lasting partnerships with our clients, helping them to
+                  not only adapt to change but to thrive in it.
                 </p>
-                <p className="opacity-75">
-                  Completely network collaborative web services via user-centric
-                  initiatives. Quickly promote sticky testing procedures before
-                  unique process improvements. Distinctively engineer innovative
-                  information whereas revolutionary process improvements. Lorem
-                  ipsum dolor sit amet, consectetur adipisicing elit. Quia enim
-                  omnis saepe dolor voluptatum. Natus soluta maxime ipsum nam
-                  sapiente dignissimos voluptatum totam. Ratione atque dolorum
-                  nostrum a est voluptatibus.
+                <p className="opacity-75 text-justify">
+                  Through our commitment to excellence in software development,
+                  IT consulting, cybersecurity, and digital marketing, we seek
+                  to inspire trust and confidence in our services. Our goal is
+                  to contribute to a sustainable and equitable digital
+                  ecosystem, where technology serves as a catalyst for growth,
+                  enabling businesses to realize their full potential while
+                  positively impacting society and the environment. Together, we
+                  will shape a future where technology empowers every
+                  organization to achieve its vision and mission.
                 </p>
               </div>
             </div>
