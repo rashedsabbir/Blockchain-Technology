2167ee66cafee81a67ba7b9948333b836e59bb3e
rashedsabbir <rashedulhasan1996@gmail.com>
2026-03-07 22:24:47 +0600

    linkedin link added in footer

--- src/components/Shared/Footer/Footer.jsx
+++ src/components/Shared/Footer/Footer.jsx
@@ -71,19 +71,40 @@ const Footer = () => {
               Aiming to please your needs as per requirements.
             </p>
           </aside>
-          <nav className="place-self-center ">
-            <div className="grid grid-flow-col gap-4">
-              <Link to="https://www.facebook.com/blockchaintechnologyltd/">
+          <nav className=" place-self-center md:justify-self-end">
+            <div className="grid grid-flow-col grid-cols-3 gap-4 place-content-center">
+              <a
+                href="https://www.facebook.com/blockchaintechnologyltd/"
+                rel="noreferrer"
+                target="_blank"
+                className="text-gray-700 transition hover:opacity-75"
+              >
+                <span className="sr-only">Facebook</span>
                 <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="32"
-                  height="32"
+                  className="size-8"
+                  fill="currentColor"
                   viewBox="0 0 24 24"
-                  className="fill-current  bg-slate-50 rounded-full"
+                  aria-hidden="true"
                 >
-                  <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
+                  <path d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"></path>
                 </svg>
-              </Link>
+              </a>
+              <a
+                href="https://www.linkedin.com/company/blockchain-technology-limited/"
+                rel="noreferrer"
+                target="_blank"
+                className="text-gray-700 transition hover:opacity-75"
+              >
+                <span className="sr-only">LinkedIn</span>
+                <svg
+                  className="size-7 "
+                  fill="currentColor"
+                  viewBox="0 0 24 24"
+                  aria-hidden="true"
+                >
+                  <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.761 0 5-2.239 5-5v-14c0-2.761-2.239-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"></path>
+                </svg>
+              </a>
             </div>
           </nav>
         </footer>
